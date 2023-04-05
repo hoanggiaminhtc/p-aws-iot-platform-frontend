c57fbaafd0e65cdde0820401a1857806cf010a59
rikkei-tiennv2 <tiennv2@rikkeisoft.com>
2023-04-05 11:17:50 +0700

    fix healthcheck 20230405 10

--- src/routes/HealthCheck.js
+++ src/routes/HealthCheck.js
@@ -1,7 +1,7 @@
 const HealthCheck = () => {
   fetch(
-    JSON.stringify(process.env.REACT_APP_API_ENDPOINT)+"/healthcheck"
-  ).then((res) => res.json())
+    JSON.stringify(process.env.REACT_APP_API_ENDPOINT)+'/healthcheck'
+  ).then((res) => res.json());
 };
 
 export default HealthCheck;
