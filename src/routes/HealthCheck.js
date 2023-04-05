439a8ca1e04f9476bf1c88223f4e90ed91f0b9b2
rikkei-tiennv2 <tiennv2@rikkeisoft.com>
2023-04-05 13:09:47 +0700

    fix healthcheck 20230405 20

--- src/routes/HealthCheck.js
+++ src/routes/HealthCheck.js
@@ -1,11 +1,13 @@
 import { useEffect } from 'react';
 const url = 'https://httpstat.us/200';
+let result=""
 const HealthCheck = () => {
   useEffect(() => {
     fetch(url).then((res) => {
-      return JSON.stringify(res.json());
+      result = JSON.stringify(res.json());
     });
   });
+  return <div>result</div>
 };
 
 export default HealthCheck;
